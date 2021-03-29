4c94b21afdfce945a064f58c67aa2786f6c040b0
sammysimmons <sammysimmons274@gmail.com>
2021-03-29 17:07:54 +0100

    services added to the hompage from database

--- home/views.py
+++ home/views.py
@@ -4,13 +4,18 @@ from products.models import Product
 # Create your views here.
 
 def index(request):
-
-    product = get_object_or_404(Product, pk=2)
-    print(product.name)
+    """ A view to show 3 standard services on the homepage to checkout """
+    product1 = get_object_or_404(Product, pk=2)
+    print(product1.name)
+    product2 = get_object_or_404(Product, pk=4)
+    print(product2.name)
+    product3 = get_object_or_404(Product, pk=1)
+    print(product3.name)
 
     context = {
-        'logoandbrand': product,
+        'logoandbrand': product1,
+        'landingpage': product3,
+        'appdesign': product2,
     }   
 
     return render(request, 'home/index.html', context)
-
