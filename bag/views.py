10d36ec9eab50d0d712470a17e5e284c4837431d
sammysimmons <sammysimmons274@gmail.com>
2021-03-30 10:24:26 +0100

    remove item from bag

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 
 def view_bag(request):
     ''' a view that renders the bag content'''
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_name):
     """ Add a product/service to bag """
@@ -19,3 +20,13 @@ def add_to_bag(request, item_name):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def remove_from_bag(request, item_name):
+    """ remove the item from bag"""
+
+    item_name in list(bag.keys())
+    bag.pop(item_name)
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
