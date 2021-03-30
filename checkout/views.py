1860ae2279e85a8fc6c7991fd0c74499336af13c
sammysimmons <sammysimmons274@gmail.com>
2021-03-30 21:24:57 +0100

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IanVEERPw2gVvEQXw4ZThIPP4Vlk8GFjyjmp6YaLV3z8EZQlLXTusKOTzjTdm62xgnwKzsAIlodt9aUNBReOA5L0030VCC1al',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
