308b9141851feae86b4894d7f0ae09e94b2cf806
sammysimmons <sammysimmons274@gmail.com>
2021-05-27 13:43:53 +0100

    emails set up

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -204,6 +204,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+#email
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'sammysimmons@example.com'
